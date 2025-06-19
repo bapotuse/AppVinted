b25586e982be157fcd69c6e1badb71547b0db73b
bapotuse <beauxbaptiste@gmail.com>
2025-06-19 07:36:48 +0200

    Ajout d'une barre de navigation

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="bg-green-200 py-4 flex px-4">
+        <router-link to="/" class="text-xl">Accueil</router-link>
+        <div class="flex-1"></div>
+        <router-link to="/article" class="text-xl">Articles</router-link>
+    </nav>
+    
+    <Home />
+</template>
+
+<script setup>
+import Home from "../components/Home.vue"
+</script>
