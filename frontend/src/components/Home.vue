3a39eca07818e207575a542c03ba37d71dacbc2f
bapotuse <beauxbaptiste@gmail.com>
2025-06-19 21:28:42 +0200

    Page d'accueil terminée

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,5 +1,29 @@
 <template>
-    <div>
-    <h1>Test</h1>
-    </div>
+  <div class="bg-green-100 min-h-screen flex flex-col justify-between">
+    <main class="flex-1 flex flex-col items-center px-4">
+      <div class="flex flex-col md:flex-row items-center gap-4">
+        <div class="text-center md:text-left px-5">
+          <h1 class="text-5xl font-bold text-gray-800">
+            Bienvenue sur la page<br>
+            des articles de <a class="hover:text-green-500"href="https://www.vinted.fr/member/89110917" target="_blank">@baptistebx</a>
+          </h1>
+        </div>
+
+        <div class="flex items-center justify-center px-5">
+            <img src="../assets/baba.jpeg" class="w-80 h-80 rounded-full border-4 border-green-500 object-cover flex items-center justify-center text-lg text-gray-700">
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
+
+<script setup>
+// Tu peux ajouter ici un composable ou une logique si besoin
+</script>
+
+<style scoped>
+
+main {
+    margin-top:10em;
+}
+</style>
